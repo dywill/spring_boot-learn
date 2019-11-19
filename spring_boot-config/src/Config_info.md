f22d245fb6c7aa358df98f5f181013a64c7538d8
dywill <2231900911@qq.com>
2019-11-19 23:57:59 +0800

    spring boot 配置相关笔记 init

---
+++ spring_boot-config/src/Config_info.md
@@ -0,0 +1,8 @@
+### Spring Boot 应用的配置
+
++ 使用 Spring Boot 创建向导创建Spring Boot 项目的说明
+    + 该向导会自动生成主程序，我们只需要编写业务逻辑即可
+    + **resources** 文件夹结构说明
+        + static: 保存所有静态资源
+        + template: 保存模板页面
+        + application.properties: Spring Boot的配置文件
---
+++ spring_boot-config/src/test/java/com/dy/spring_boot/SpringBootConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
